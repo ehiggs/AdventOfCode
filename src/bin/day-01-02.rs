212be48ce9daed663636d1f69e0fa31c39285bf9
Ewan Higgs <ewan.higgs@gmail.com>
2022-12-05 05:25:12 +0100

    Initial commit.

---
+++ src/bin/day-01-02.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: day-01 <input-file>");
+        std::process::exit(1);
+    }
+    let input = &args[1];
+    let file = File::open(input).unwrap();
+    let reader = BufReader::new(file);
+    let mut max_elves: [usize;3] = [0, 0, 0];
+    let mut current_elf : usize = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+
+        if line == "" { 
+            register_elf(&mut max_elves, current_elf);
+            current_elf = 0;
+        } else {
+            let fruit : usize = line.parse::<usize>().unwrap();
+            current_elf += fruit;
+        }
+    }
+    register_elf(&mut max_elves, current_elf);
+    let sum = max_elves.iter().sum::<usize>();
+    println!("Max elf has {:?} with sum {}", max_elves, sum);
+}
+
+fn register_elf(elves: &mut [usize;3], elf: usize) {
+    for i in 0..2 {
+        if elf > elves[i] {
+            elves[i] = elf;
+            elves.sort();
+            println!("{:?}", elves);
+            return
+        }
+    }
+}
---
+++ src/bin/day-02-02.rs
@@ -0,0 +1,106 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+enum Move {
+    Rock,
+    Paper,
+    Scissor,
+}
+#[derive(Debug)]
+struct Game {
+    my_move: Move,
+    opponent_move: Move,
+}
+
+fn parse_game(line: &str) -> Game {
+    let opponent_move = match line.chars().nth(0) {
+        Some('A') => Move::Rock,
+        Some('B') => Move::Paper,
+        Some('C') => Move::Scissor,
+        _ => unimplemented!(),
+    };
+    let my_move = match (line.chars().nth(2), &opponent_move) {
+        (Some('X'), Move::Rock) => Move::Scissor,
+        (Some('X'), Move::Paper) => Move::Rock,
+        (Some('X'), Move::Scissor) => Move::Paper,
+        (Some('Y'), Move::Rock) => Move::Rock,
+        (Some('Y'), Move::Paper) => Move::Paper,
+        (Some('Y'), Move::Scissor) => Move::Scissor,
+        (Some('Z'), Move::Rock) => Move::Paper,
+        (Some('Z'), Move::Paper) => Move::Scissor,
+        (Some('Z'), Move::Scissor) => Move::Rock,
+        _ => unimplemented!(),
+    };
+    Game {
+        my_move,
+        opponent_move,
+    }
+}
+
+impl Game {
+    /// 0 points loss; 3 points draw, 6 points win
+    /// 1 point rock, 2 points paper, 3 points scissors
+    fn score(&self) -> usize {
+        match self {
+            Game {
+                my_move: Move::Rock,
+                opponent_move: Move::Rock,
+            } => 1 + 3,
+            Game {
+                my_move: Move::Paper,
+                opponent_move: Move::Rock,
+            } => 2 + 6,
+            Game {
+                my_move: Move::Scissor,
+                opponent_move: Move::Rock,
+            } => 3 + 0,
+            Game {
+                my_move: Move::Rock,
+                opponent_move: Move::Paper,
+            } => 1 + 0,
+            Game {
+                my_move: Move::Paper,
+                opponent_move: Move::Paper,
+            } => 2 + 3,
+            Game {
+                my_move: Move::Scissor,
+                opponent_move: Move::Paper,
+            } => 3 + 6,
+            Game {
+                my_move: Move::Rock,
+                opponent_move: Move::Scissor,
+            } => 1 + 6,
+            Game {
+                my_move: Move::Paper,
+                opponent_move: Move::Scissor,
+            } => 2 + 0,
+            Game {
+                my_move: Move::Scissor,
+                opponent_move: Move::Scissor,
+            } => 3 + 3,
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: day-01 <input-file>");
+        std::process::exit(1);
+    }
+    let input = &args[1];
+    let file = File::open(input).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut total_score = 0;
+    for (i, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        let game = parse_game(&line);
+        let score = game.score();
+        println!("Game {} {:?} score: {}", i, game, score);
+        total_score += score;
+    }
+    println!("Total score is {}", total_score);
+}
